7e343fef0d9c50bafce0e8a4313eeabefd966565
Mohammad Alamas <mohammad4atieh@gmail.com>
2026-04-09 13:29:27 +0300

    chore: test slack notification

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ export default function App() {
     <div className="flex h-screen items-center justify-center bg-slate-900 text-white">
       <div className="text-center">
         <h1 className="text-9xl font-black text-red-600 underline">UniShare</h1>
+        <h3 className="text-9xl font-black text-blue-600 underline">Hi</h3>
         <p className="mt-4 text-xl text-slate-400">Graduation Project Starts Now.</p>
       </div>
     </div>
