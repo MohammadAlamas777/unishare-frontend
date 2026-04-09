8974c7af3376280e387265a2b82b6c7fb6b3c131
Mohammad Alamas <mohammad4atieh@gmail.com>
2026-04-09 13:19:31 +0300

    feat: first stable build with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900 text-white">
+      <div className="text-center">
+        <h1 className="text-9xl font-black text-red-600 underline">UniShare</h1>
+        <p className="mt-4 text-xl text-slate-400">Graduation Project Starts Now.</p>
+      </div>
+    </div>
+  )
+}
